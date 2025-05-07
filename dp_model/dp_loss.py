ed8e748f30bb793e7005017caf743b3c1fb6ccce
Heke11 <147317795+Heke11@users.noreply.github.com>
2025-05-07 09:36:36 +0800

    Update dp_loss.py

--- dp_model/dp_loss.py
+++ dp_model/dp_loss.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 
-def my_KLDivLoss(x, y):
+def my_KLDivLoss(x, y):            # x,y都是[batch_size, 40]
     """Returns K-L Divergence loss
     Different from the default PyTorch nn.KLDivLoss in that
     a) the result is averaged by the 0th dimension (Batch size)
@@ -8,7 +8,7 @@ def my_KLDivLoss(x, y):
     """
     loss_func = nn.KLDivLoss(reduction='sum')
     y += 1e-16
-    n = y.shape[0]
+    n = y.shape[0]            # n是批量大小
     loss = loss_func(x, y) / n
     #print(loss)
     return loss
